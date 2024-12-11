ad8d25a691a520b9e67c9cfe5dbdc2980a0b9a89
minseon23 <53747428+username0802@users.noreply.github.com>
2024-12-11 17:21:59 +0900

    12/11 5pm update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from openai import OpenAI
+import bing
 
 # Initialize the chat list in session state
 if "chat_list" not in st.session_state:
@@ -23,6 +24,7 @@ with st.sidebar:
     new_chat = st.button('Start!')
 
     openai_api_key = st.text_input("OpenAI API Key", type="password")
+    bing_api_key = st.text_input("bing API Key", type="password")
 
     if new_chat:
         # Add the new chat to the list if the topic is not empty
@@ -44,56 +46,118 @@ if selected_chat == 'main' :
     "Make new chatting with left header."
     "You can input the topic you are interested in, and you can choose your language level."
     )
+    
 
 else :
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+    chat, option, fixing = st.tabs(['chatting', 'option', 'fixed inputs'])
 
-    # This is where you would add the functionality for sending/new messages, etc.
-    else :
-        # Create an OpenAI client.
-        client = OpenAI(api_key=openai_api_key)
+    with chat :
 
-        # Create a session state variable to store the chat messages. This ensures that the
-        # messages persist across reruns.
         if "messages" not in st.session_state: #['beginner', 'intermediate', 'advanced']
             st.session_state.messages = {}
+            st.session_state.fix_messages = {}
         if selected_chat not in st.session_state.messages :
-            text = f'You are chatting with user in context of '+ selected_chat
+            text = f'you are chatting with user in context of '+ selected_chat
             if (st.session_state.chat_list_option[selected_chat]['level'] == 'beginner') :
                 text += '. Talk with user in English, and suggest better chance for user to speak ' +st.session_state.chat_list_option[selected_chat]['language']
             else :
-                text += '. Talk with user in '+st.session_state.chat_list_option[selected_chat]['language']+', and make user\'s message more natural'
-            text += '. Make sure you search about newest information about ' + selected_chat + '.'
+                text += '. Talk with user in '+st.session_state.chat_list_option[selected_chat]['language']
+            text += ". Answer with a length similar to the user's input."
             st.session_state.messages[selected_chat] = [{'role':'system', 'content' : text}]
-            st.write('prompt : ' + text)
 
-        # Display the existing chat messages via `st.chat_message`.
-        for message in st.session_state.messages[selected_chat][1:]:
+            if st.session_state.chat_list_option[selected_chat]['level'] == 'beginner' :
+                fixing_require = f"You are translator. Translate input texts in {st.session_state.chat_list_option[selected_chat]['language']}"
+            else :
+                fixing_require = "You are writing assistant. You are not supposed to answer the question, but fixing the inpuy text. DON\'T ANSWER THE QUESTION. JUST FIX IT."
+            st.session_state.fix_messages[selected_chat] = [{'role':'system', 'content' : fixing_require}]
+
+
+
+            # Display the existing chat messages via `st.chat_message`.
+            for message in st.session_state.messages[selected_chat][1:]:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+
+        if not openai_api_key or not bing_api_key:
+            st.info("Please add your API key to continue.", icon="🗝️")
+        # This is where you would add the functionality for sending/new messages, etc.
+        else :
+            # Create an OpenAI client.
+            client = OpenAI(api_key=openai_api_key)
+
+
+            for message in st.session_state.messages[selected_chat][1:]:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+            # Create a session state variable to store the chat messages. This ensures that the
+            # messages persist across reruns.
+
+            # Create a chat input field to allow the user to enter a message. This will display
+            # automatically at the bottom of the page.
+            if prompt := st.chat_input("What is up?"):
+
+                result_texts = bing.get_relevant_texts(f"{selected_chat} {prompt}", bing_api_key)
+
+                # Store and display the current prompt.
+                prompt_with_context = f"""
+                here is the context about {selected_chat}.
+
+                answer in context below :
+                {str(result_texts)}
+
+                Question : 
+                {prompt}
+                """
+
+                with st.chat_message("user"):
+                    st.markdown(prompt)
+
+                st.session_state.fix_messages[selected_chat].append({"role": "user", "content": prompt})
+                st.session_state.messages[selected_chat].append({"role": "user", "content": prompt_with_context})
+
+                fix_stream = client.chat.completions.create(
+                    model="gpt-4o-mini",
+                    messages=[
+                        {"role": m["role"], "content": m["content"]}
+                        for m in st.session_state.fix_messages[selected_chat]
+                    ],
+                    stream=True,
+                )
+
+                with st.chat_message("assistant"):
+                    response = st.write_stream(fix_stream)
+                st.session_state.fix_messages[selected_chat].append({"role": "assistant", "content": response})
+
+
+                # Generate a response using the OpenAI API.
+                
+                stream = client.chat.completions.create(
+                    model="gpt-4o-mini",
+                    messages=[
+                        {"role": m["role"], "content": m["content"]}
+                        for m in st.session_state.messages[selected_chat]
+                    ],
+                    stream=True,
+                )
+
+                # Stream the response to the chat using `st.write_stream`, then store it in 
+                # session state.
+
+                with st.chat_message("assistant"):
+                    response = st.write_stream(stream)
+                st.session_state.messages[selected_chat].append({"role": "assistant", "content": response})
+                st.session_state.messages[selected_chat][-2]['content'] = prompt
+
+                
+
+
+    with option :
+        st.write(st.session_state.messages[selected_chat][0]['content'])
+        fixed_prompt = st.text_input("fix your prompt")
+        if fixed_prompt :
+            st.session_state.messages[selected_chat][0]['content'] += fixed_prompt
+
+    with fixing :
+        for message in st.session_state.fix_messages[selected_chat][1:]:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
-
-        # Create a chat input field to allow the user to enter a message. This will display
-        # automatically at the bottom of the page.
-        if prompt := st.chat_input("What is up?"):
-
-            # Store and display the current prompt.
-            st.session_state.messages[selected_chat].append({"role": "user", "content": prompt})
-            with st.chat_message("user"):
-                st.markdown(prompt)
-
-            # Generate a response using the OpenAI API.
-            stream = client.chat.completions.create(
-                model="gpt-4o-mini",
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages[selected_chat]
-                ],
-                stream=True,
-            )
-
-            # Stream the response to the chat using `st.write_stream`, then store it in 
-            # session state.
-            with st.chat_message("assistant"):
-                response = st.write_stream(stream)
-            st.session_state.messages[selected_chat].append({"role": "assistant", "content": response})
